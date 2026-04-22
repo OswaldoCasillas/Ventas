e0af15dbfeec35ba076eb465f09469fa0faf1808
Oswaldo Casillas <oswaldocasillas1@gmail.com>
2026-04-22 00:54:47 -0600

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,5 @@
-// Config común para todas las páginas
 window.APP_CONFIG = {
-  ORG:  "OswaldoCasillas",
+  ORG: "OswaldoCasillas",
   REPO: "Ventas",
-  API_URL: "https://ventas-backend-rose.vercel.app/api/create-issue" // tu Vercel ✅
+  API_URL: "https://ventas-backend-rose.vercel.app/api/create-issue"
 };
